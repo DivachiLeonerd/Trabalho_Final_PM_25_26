5bb0d6366a73412fdadc71bc26f1e4a9cb3345ce
Afonso Fernandes <81565090+DivachiLeonerd@users.noreply.github.com>
2025-11-17 20:04:47 +0000

    read_next_member was added and tested

--- Test.c
+++ Test.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #define MAX_LINE_LENGTH 170
+#define MAX_MEMBER_LENGTH 50
 
 void read_next_member(char *member, char *line)
 {
@@ -9,32 +10,37 @@ void read_next_member(char *member, char *line)
 
         i = 0;
         if (j >= MAX_LINE_LENGTH)
-                return ;
-        while (i < 50)
         {
-                member[i++] = '0';
+                j = 0;
+        }
+        while (i < 50)
+        {            
+                member[i++] = '\0';
         }
         i = 0;
         while (1)
         {
-                if (line[j] == '\n')
+                if (line[j] == '\n' || line[j] == EOF || line[j] == '\0' || line[j] == ';')
                 {
                         j++;
                         return ;
                 }
+                //printf("valor de indice j:%d vai para membro indice i:%d\n", j ,i);
                 member[i++] = line[j++];
         }
 }
 
 int main()
 {
-        char    line[MAX_LINE_LENGTH] = "Esta linha é\ndo caraças";
+        FILE    *db_text;
+        char    line[50];
         char    member[50];
 
         member[49] = '\0';
-
+        db_text = fopen("./db.txt", "r");
         printf("hello World\n");
 
+        fgets(line, MAX_MEMBER_LENGTH, db_text);
         read_next_member(member, line);
         printf("***%s***\n", member);
         read_next_member(member, line);
--- main.c
+++ main.c
@@ -3,6 +3,16 @@
 
 #define MAX_LINE_LENGTH 170 //SUM of max field width + all the ';'
 #define MAX_MEMBER_COUNTER 9
+#define MAX_MEMBER_LENGTH 50
+#define CALLER_ID_LENGTH 14
+#define NAME_LENGTH 50
+#define DATE_LENGTH 10
+#define TIMESTAMP_LENGTH 11
+#define REGION_CD_LENGTH 1
+
+
+
+
 typedef struct
 {
 	char	caller_id[14];
@@ -16,54 +26,57 @@ typedef struct
 	char	client_zone;
 }CDR;
 
-char *read_next_member(char *line)
+void read_next_member(char *member, char *line)
+{
+        short int               i;
+        static short int        j = 0;
+
+        i = 0;
+        if (j >= MAX_LINE_LENGTH)
+        {
+            j = 0;
+        }
+        while (i < MAX_MEMBER_LENGTH)
+        {            
+            member[i++] = '\0';
+        }
+        i = 0;
+        while (1)
+        {
+			if (line[j] == '\n' || line[j] == EOF || line[j] == '\0' || line[j] == ';')
+			{
+				j++;
+				return ;
+			}
+			member[i++] = line[j++];
+        }
+	return ;
+}
+
+CDR	*read_call_record(CDR *new_CDR, char *line)//reads and initializes a call_record, returns DB's address or null whether it was successful or not
 {
 	char				member[50];
 	short int			i;
-	static short int	j = 0;
+	static short int	member_counter = 0;
 
 	i = 0;
-	if (j >= MAX_LINE_LENGTH)
-		return null;
-	while (i < 50)
-	{
-		member[i++] = '0';
-	}
-	i = 0;
-	while (1)
-	{
-		if (line[j] == '\n')
-		{
-			j++;
-			return (member);
-		}
-		member[i] = line[j];
-	}
-}
-
-void read_call_record(CDR *DB, char *line)
-{
-	short int	i;
-	short int	member_counter;
-	char		*member;
-
-	i = 0;
-	member_counter = 0;
+	if (member_counter >= MAX_MEMBER_COUNTER)
+		member_counter = 0;
 	while (member_counter < MAX_MEMBER_COUNTER)
 	{
 		//call a function that gets the next member
-		member = read_next_member(line);
+		read_next_member(member, line);
 		switch (member_counter)
 		{
-			case 0: 
-			case 1:
-			case 2:
-			case 3:
-			case 4:
-			case 5:
-			case 6:
-			case 7:
-			case 8:
+			case 0:strncpy(new_CDR->caller_id, member, CALLER_ID_LENGTH); break; 
+			case 1:strncpy(new_CDR->caller_name, member, NAME_LENGTH); break;
+			case 2:strncpy(new_CDR->caller_id, member, CALLER_ID_LENGTH); break;
+			case 3:strncpy(new_CDR->client_name, member, NAME_LENGTH); break;
+			case 4:strncpy(new_CDR->start_date, member, DATE_LENGTH); break;
+			case 5:strncpy(new_CDR->start_time, member, TIMESTAMP_LENGTH); break;
+			case 6:strncpy(new_CDR->end_time, member, TIMESTAMP_LENGTH); break;
+			case 7:strncpy(new_CDR->caller_zone, member, REGION_CD_LENGTH); break;
+			case 8:strncpy(new_CDR->client_zone, member, REGION_CD_LENGTH); break;
 		}
 	}
 }
