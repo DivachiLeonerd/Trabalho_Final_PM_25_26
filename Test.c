20202b9908e26f28b33be933f9a7b1cb175e360b
FERNANDESA8 <81565090+DivachiLeonerd@users.noreply.github.com>
2025-11-17 15:11:31 +0000

    Update Test.c

--- Test.c
+++ Test.c
@@ -1,39 +1,44 @@
 #include <stdio.h>
 #include <string.h>
+#define MAX_LINE_LENGTH 170
 
-
-char *read_next_member(char *line)
+void read_next_member(char *member, char *line)
 {
-	char				member[50];
-	short int			i;
-	static short int	j = 0;
+        short int               i;
+        static short int        j = 0;
 
-	i = 0;
-	if (j >= MAX_LINE_LENGTH)
-		return null;
-	while (i < 50)
-	{
-		member[i++] = '0';
-	}
-	i = 0;
-	while (1)
-	{
-		if (line[j] == '\n')
-		{
-			j++;
-			return (member);
-		}
-		member[i] = line[j];
-	}
+        i = 0;
+        if (j >= MAX_LINE_LENGTH)
+                return ;
+        while (i < 50)
+        {
+                member[i++] = '0';
+        }
+        i = 0;
+        while (1)
+        {
+                if (line[j] == '\n')
+                {
+                        j++;
+                        return ;
+                }
+                member[i++] = line[j++];
+        }
 }
 
 int main()
 {
-	char	line = "Esta linha é\ndo caralho";
-	char	*read_line;
-	read_line = read_next_member(line);
-	printf("***%s***\n", read_line);
-	
-	
-	return 0;
+        char    line[MAX_LINE_LENGTH] = "Esta linha é\ndo caraças";
+        char    member[50];
+
+        member[49] = '\0';
+
+        printf("hello World\n");
+
+        read_next_member(member, line);
+        printf("***%s***\n", member);
+        read_next_member(member, line);
+        printf("***%s***\n", member);
+
+        return 0;
 }
