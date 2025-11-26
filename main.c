e5a10c2fcc0ae2f55856447d952b65433822a2cb
Afonso Fernandes <81565090+DivachiLeonerd@users.noreply.github.com>
2025-11-26 19:02:24 +0000

    deleted files + 1st prototype of initialize_DB() done
    Put everything in main from test.c as it doesn't make much sense for me to manually pass changes from one file to another

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 
-#define MAX_LINE_LENGTH 170 //SUM of max field width + all the ';'
+#define MAX_LINE_LENGTH 173 //SUM of max field width + all the ';' + '\n'
 #define MAX_MEMBER_COUNTER 9
 #define MAX_MEMBER_LENGTH 50
 #define CALLER_ID_LENGTH 14
@@ -9,19 +9,19 @@
 #define DATE_LENGTH 10
 #define TIMESTAMP_LENGTH 11
 #define REGION_CD_LENGTH 1
-
-
+#define MAX_CDR_COUNT 1000000
+#define FILENAME "./db.txt"
 
 
 typedef struct
 {
-	char	caller_id[14];
-	char	caller_name[50];
-	char	client_id[14];
-	char	client_name[50];
-	char	start_date[10];
-	char	start_time[11];
-	char	end_time[11];
+	char	caller_id[CALLER_ID_LENGTH];
+	char	caller_name[NAME_LENGTH];
+	char	client_id[CALLER_ID_LENGTH];
+	char	client_name[NAME_LENGTH];
+	char	start_date[DATE_LENGTH];
+	char	start_time[TIMESTAMP_LENGTH];
+	char	end_time[TIMESTAMP_LENGTH];
 	char	caller_zone;
 	char	client_zone;
 }CDR;
@@ -41,15 +41,23 @@ void print_chr_array(char *array, int array_size)
 void print_CDR(CDR *call_record)
 {
         print_chr_array(call_record->caller_id, CALLER_ID_LENGTH);
+        putchar('-');
         print_chr_array(call_record->caller_name, NAME_LENGTH);
+        putchar('-');
         print_chr_array(call_record->client_id, CALLER_ID_LENGTH);
+        putchar('-');
         print_chr_array(call_record->client_name, NAME_LENGTH);
+        putchar('-');
         print_chr_array(call_record->start_date, DATE_LENGTH);
+        putchar('-');
         print_chr_array(call_record->start_time, TIMESTAMP_LENGTH);
+        putchar('-');
         print_chr_array(call_record->end_time, TIMESTAMP_LENGTH);
+        putchar('-');
         print_chr_array(&(call_record->caller_zone), REGION_CD_LENGTH);
+        putchar('-');
         print_chr_array(&(call_record->client_zone), REGION_CD_LENGTH);
-
+        putchar('\n');
         return ;
 }
 
@@ -59,7 +67,7 @@ void read_next_member(char *member, char *line)
         static short int        j = 0;
 
         i = 0;
-        if (j >= MAX_LINE_LENGTH)
+        if (j >= MAX_LINE_LENGTH - 2)
         {
                 j = 0;
         }
@@ -75,23 +83,23 @@ void read_next_member(char *member, char *line)
                         j++;
                         return ;
                 }
-                //printf("\nvalor de indice j:%d(%c) vai para membro indice i:%d\n", j, line[j] ,i);
                 member[i++] = line[j++];
         }
 }
 
 CDR	*read_call_record(CDR *new_CDR, char *line)//reads and initializes a call_record, returns DB's address or null whether it was successful or not
 {
-	char				member[MAX_MEMBER_LENGTH];
+	char			member[MAX_MEMBER_LENGTH];
 	static short int	member_counter = 0;
 
 	if (member_counter >= MAX_MEMBER_COUNTER)
 	        member_counter = 0;
+        // printf("read_call_record():line:%s\n", line);
 	while (member_counter < MAX_MEMBER_COUNTER)
 	{
 		//call a function that gets the next member
-		read_next_member(member, line);
-                //printf("\nread_call_record()-member:%s\nmember_counter:%d\n", member, member_counter);
+                read_next_member(member, line);
+                // printf("read_call_record(): %s\nmember counter:%d\n", member, member_counter);
 		switch (member_counter)
 		{
 			case 0:strncpy(new_CDR->caller_id, member, CALLER_ID_LENGTH); break;
@@ -119,12 +127,31 @@ void initialize_DB(CDR *DB)
 {
 	char	line[MAX_LINE_LENGTH];
 	FILE	*f_stream;
+        int     i;
+        CDR    *new_CDR;
+        char    *result;
 
-	while ((line = fgets(f_stream,MAX_LINE_LENGTH, f_stream)) != EOF)
-	{
-		read_call_record(DB, line);
+        i = 0;
 
-	}
+        f_stream = fopen(FILENAME, "r");
+        do
+        {
+                result = fgets(line, MAX_LINE_LENGTH, f_stream);
+                //printf("Initialize_DB(): line:*%d* || result:%p\n", line[0], result);
+                if (result == NULL)
+                        break ;
+                while (line[0] == '\n')
+                {
+                        result = fgets(line, 1, f_stream); 
+                }
+                //printf("After Initialize_DB(): line:*%s* || result:%p\n", line, result);
+                new_CDR = read_call_record(&(DB[i]), line);
+                print_CDR(new_CDR);
+                i++;
+        }
+	while (line != NULL || new_CDR != NULL);
+        fclose(f_stream);
+        return ;
 }
 
 main_loop(CDR *DB)
@@ -132,11 +159,38 @@ main_loop(CDR *DB)
 	
 }
 
-
-int main(int argc, char *argv)
+int main(int argc, char **argv)
 {
-	static CDR DB[1000000];
+        static CDR      DB[MAX_CDR_COUNT];
+        FILE            *file_stream;
+        char            buff[MAX_LINE_LENGTH];
+        int             i;
 
-	main_loop(DB);
-	return 0;
+        i = 0;
+        file_stream = fopen(FILENAME, "r");
+        if (file_stream == NULL)
+                return 1;
+        printf("hello World\n");
+        initialize_DB(DB);
+        // for (int i = 0; i < 25; i++)
+        // {
+        //         read_call_record(&(DB[i]), buff);
+        //         print_CDR(&(DB[i]));
+        // }
+        // while (i < 5)
+        // {
+        //         print_CDR(&(DB[i++]));
+        // }
+        return 0;
 }
+
+
+
+
+// int main(int argc, char *argv)
+// {
+// 	static CDR DB[MAX_CDR_COUNT];
+
+// 	main_loop(DB);
+// 	return 0;
+// }
